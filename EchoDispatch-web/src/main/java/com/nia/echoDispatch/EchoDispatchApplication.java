84e3f3cebab9dc6312d18849efbe651c8cabdf16
0125nia <1205367274@qq.com>
2024-05-05 15:26:25 +0800

    引入Spring并测试

---
+++ EchoDispatch-web/src/main/java/com/nia/echoDispatch/EchoDispatchApplication.java
@@ -0,0 +1,11 @@
+package com.nia.echoDispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoDispatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EchoDispatchApplication.class,args);
+    }
+}
---
+++ EchoDispatch-web/src/main/java/com/nia/echoDispatch/web/TestController.java
@@ -0,0 +1,12 @@
+package com.nia.echoDispatch.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    private String test(){
+        return "testing";
+    }
+}
