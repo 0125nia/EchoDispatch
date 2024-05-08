132f82883d09291992c36ac811f0ba4245ef860b
0125nia <1205367274@qq.com>
2024-05-08 17:35:24 +0800

    引入日志框架

--- EchoDispatch-web/src/main/java/com/nia/echoDispatch/web/TestController.java
+++ EchoDispatch-web/src/main/java/com/nia/echoDispatch/web/TestController.java
@@ -1,12 +1,16 @@
 package com.nia.echoDispatch.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
     @RequestMapping("/test")
     private String test(){
+        System.out.println("sout输出");
+        log.info("log:{}","日志输出");
         return "testing";
     }
 }
